806ecb4c3813310bbbc9d3af6c5bf9710ef78a48
Vitaliy Siukayev <vsiukayev@corevalue.net>
2020-02-29 17:23:10 +0200

    Clean up

--- src/aura/auraPubsubSubscriber/auraPubsubSubscriberController.js
+++ src/aura/auraPubsubSubscriber/auraPubsubSubscriberController.js
@@ -3,7 +3,6 @@
         var pubsub = component.find('pubsub');
         var callback = $A.getCallback(function(eventData) {
             component.set('v.lwcEventData', JSON.stringify(eventData));
-            //eventData.firstName = 'Bart';
         });
         pubsub.registerListener('lwcButtonClick', callback);
     },
