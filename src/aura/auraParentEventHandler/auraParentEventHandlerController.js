9ea30a17a8d3ee34d748878d64a925c0b900ebae
Vitaliy Siukayev <vsiukayev@corevalue.net>
2020-03-05 09:15:57 +0200

    added nested object example

--- src/aura/auraParentEventHandler/auraParentEventHandlerController.js
+++ src/aura/auraParentEventHandler/auraParentEventHandlerController.js
@@ -2,8 +2,12 @@
 ({
     handleEvent: function(component, event, helper) {
         var param = event.getParam('param');
-        //param.firstName = 'Lisa';
-        //param.lastName = 'Simpson';
+
+        param.firstName = 'Lisa';
+        param.lastName = 'Simpson';
+        param.additional.age = 11;
+        param.additional.gender = 'female';
+
         component.set('v.auraParams', param);
     }
 });
--- src/lwc/lwcChildEvent/lwcChildEvent.js
+++ src/lwc/lwcChildEvent/lwcChildEvent.js
@@ -2,10 +2,18 @@
 import { LightningElement, track } from 'lwc';
 
 export default class LwcChildEvent extends LightningElement {
-    @track param = {firstName: 'John', lastName: 'Week'};
+    @track param = {
+        firstName: 'John',
+        lastName: 'Week',
+        additional: {
+            age: 23,
+            gender: 'male'
+        }
+    };
 
     handleButtonClick() {
-        const param = this.param;
+        const param = {...this.param};
+        param.additional = {...this.param.additional};
         this.dispatchEvent(
             new CustomEvent('customevent', {
                 detail: { param }
